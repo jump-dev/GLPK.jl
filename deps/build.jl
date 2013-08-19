a6de80db82869a4d4602dc2dff2abb38a88db51c
Carlo Baldassi <carlobaldassi@gmail.com>
2013-08-19 16:30:43 +0200

    Add homebrew provider
    Fixes #3?

--- deps/build.jl
+++ deps/build.jl
@@ -10,6 +10,8 @@ glpkdep = library_dependency("libglpk", validate = ((name,handle)->(bytestring(c
 provides(Sources, {URI("http://ftp.gnu.org/gnu/glpk/$glpkname.tar.gz") => glpkdep}, os = :Unix)
 provides(Sources, {URI("http://downloads.sourceforge.net/project/winglpk/winglpk/GLPK-$glpkvers/win$glpkname.zip") => glpkdep}, os = :Windows)
 
+provides(Homebrew, {"libglpk" => glpkdep})
+
 provides(BuildProcess, {
     Autotools(libtarget = joinpath("src", ".libs", "libglpk.la"), configure_options = String["--with-gmp", "--enable-dl"]) => glpkdep
     }, os = :Unix)
