40bbddd2a2badb107e1bec93c977f8ecec022704
Carlo Baldassi <carlobaldassi@gmail.com>
2013-09-09 10:19:40 +0200

    add! -> push! + cosmetics

--- deps/build.jl
+++ deps/build.jl
@@ -11,23 +11,23 @@ provides(Sources, {URI("http://ftp.gnu.org/gnu/glpk/$glpkname.tar.gz") => glpkde
 provides(Sources, {URI("http://downloads.sourceforge.net/project/winglpk/winglpk/GLPK-$glpkvers/win$glpkname.zip") => glpkdep}, os = :Windows)
 
 @osx_only begin
-  if( Pkg.installed("Homebrew") === nothing )
-    error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
-  else
-    using Homebrew
-    provides( Homebrew.HB, "glpk", glpkdep, os = :Darwin )
-  end
+    if( Pkg.installed("Homebrew") === nothing )
+        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+    else
+        using Homebrew
+        provides( Homebrew.HB, "glpk", glpkdep, os = :Darwin )
+    end
 end
 
-julia_usrdir = normpath(JULIA_HOME*"/../") # This is a stopgap, we need a better builtin solution to get the included libraries
+julia_usrdir = normpath("$JULIA_HOME/../") # This is a stopgap, we need a better builtin solution to get the included libraries
 libdirs = String["$(julia_usrdir)/lib"]
 includedirs = String["$(julia_usrdir)/include"]
 
 provides(BuildProcess, {
-    Autotools(libtarget = joinpath("src", ".libs", "libglpk.la"), 
+    Autotools(libtarget = joinpath("src", ".libs", "libglpk.la"),
               # configure_options = String["--with-gmp", "--enable-dl"],
               configure_options = String["--with-gmp"],
-              lib_dirs = libdirs, 
+              lib_dirs = libdirs,
               include_dirs = includedirs) => glpkdep
     }, os = :Unix)
 
