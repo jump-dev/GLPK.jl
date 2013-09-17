d07af7b986b101201cf2cdd855d4ed625e3a28d4
Carlo Baldassi <carlobaldassi@gmail.com>
2013-09-17 02:14:38 +0200

    Tentative Windows support

--- deps/build.jl
+++ deps/build.jl
@@ -4,12 +4,26 @@ using BinDeps
 
 glpkvers = "4.52"
 glpkname = "glpk-$glpkvers"
+glpkdllname = "glpk_$(replace(glpkvers, ".", "_"))"
 
-glpkdep = library_dependency("libglpk", validate = ((name,handle)->(bytestring(ccall(dlsym(handle, :glp_version), Ptr{Uint8}, ())) == glpkvers)))
+glpkvalidate(name, handle) = (bytestring(ccall(dlsym(handle, :glp_version), Ptr{Uint8}, ())) == glpkvers)
+glpkdep = library_dependency("libglpk", aliases = [glpkdllname], validate = glpkvalidate)
+
+# Build from sources (used by Linux, BSD)
+julia_usrdir = normpath("$JULIA_HOME/../") # This is a stopgap, we need a better builtin solution to get the included libraries
+libdirs = String["$(julia_usrdir)/lib"]
+includedirs = String["$(julia_usrdir)/include"]
 
 provides(Sources, {URI("http://ftp.gnu.org/gnu/glpk/$glpkname.tar.gz") => glpkdep}, os = :Unix)
-provides(Sources, {URI("http://downloads.sourceforge.net/project/winglpk/winglpk/GLPK-$glpkvers/win$glpkname.zip") => glpkdep}, os = :Windows)
+provides(BuildProcess, {
+    Autotools(libtarget = joinpath("src", ".libs", "libglpk.la"),
+              configure_options = String["--with-gmp"],
+              lib_dirs = libdirs,
+              include_dirs = includedirs) => glpkdep
+    }, os = :Unix)
 
+
+# Homebrew (OS X section)
 @osx_only begin
     if( Pkg.installed("Homebrew") === nothing )
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
@@ -19,16 +33,26 @@ provides(Sources, {URI("http://downloads.sourceforge.net/project/winglpk/winglpk
     end
 end
 
-julia_usrdir = normpath("$JULIA_HOME/../") # This is a stopgap, we need a better builtin solution to get the included libraries
-libdirs = String["$(julia_usrdir)/lib"]
-includedirs = String["$(julia_usrdir)/include"]
-
-provides(BuildProcess, {
-    Autotools(libtarget = joinpath("src", ".libs", "libglpk.la"),
-              # configure_options = String["--with-gmp", "--enable-dl"],
-              configure_options = String["--with-gmp"],
-              lib_dirs = libdirs,
-              include_dirs = includedirs) => glpkdep
-    }, os = :Unix)
+# Windows
+glpklibdir = BinDeps.libdir(glpkdep)
+glpksrcdir = BinDeps.srcdir(glpkdep)
+glpkdownloadsdir = BinDeps.downloadsdir(glpkdep)
+println(glpksrcdir)
+glpkdlldir = joinpath(glpksrcdir, glpkname, "w$WORD_SIZE")
+provides(SimpleBuild,
+    (@build_steps begin
+        FileDownloader("http://downloads.sourceforge.net/project/winglpk/winglpk/GLPK-$glpkvers/win$glpkname.zip",
+                       joinpath(glpkdownloadsdir, "win$glpkname.zip"))
+        CreateDirectory(glpksrcdir, true)
+        FileUnpacker(joinpath(glpkdownloadsdir, "win$glpkname.zip"),
+                     glpksrcdir, glpkdlldir)
+        CreateDirectory(glpklibdir, true)
+        @build_steps begin
+            ChangeDirectory(glpkdlldir)
+            FileRule(joinpath(glpklibdir, "$glpkdllname.dll"), @build_steps begin
+                `cp $glpkdllname.dll $glpklibdir`
+            end)
+        end
+    end), glpkdep, os = :Windows)
 
 @BinDeps.install
