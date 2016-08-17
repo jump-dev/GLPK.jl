34311c4d6291e506fea9fbb5eb633f02d8d8d113
Miles Lubin <miles.lubin@gmail.com>
2016-08-17 15:40:47 -0600

    bump to GLPK 4.61 on linux

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,7 @@
 module GLPK_tests
 
-# backwards-compatible test_throws (works in julia 0.2)
 macro test_throws_02(args...)
-    if VERSION >= v"0.3-"
-        :(@test_throws($(esc(args[1])), $(esc(args[2]))))
-    else
-        :(@test_throws($(esc(args[2]))))
-    end
+    :(@test_throws($(esc(args[1])), $(esc(args[2]))))
 end
 
 macro glpk_test_throws(args)
