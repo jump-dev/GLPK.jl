dfdbb65cdb8e37191e1122df8f6679302c5b0164
odow <o.dowson@gmail.com>
2019-02-10 19:42:54 -0600

    Unify structure with Gurobi

--- src/GLPKExtensions.jl
+++ src/GLPKExtensions.jl
@@ -3,15 +3,29 @@ module GLPKExtensions
 using GLPK, JuMP
 
 export @lazy_constraint
+export set_callbacks, add_lazy_constraint, add_heuristic_solution
 
-macro lazy_constraint(cb_data, expr)
+"""
+    @lazy_constraint(model, cb_data, expr)
+
+Add a lazy constraint (where the constraint is given by `expr`) to `model`.
+
+This can be called *only* from a lazy callback set by `JuMP.set_callbacks`.
+
+### Examples
+
+    @lazy_constraint(model, cb_data, 2x + y <+ 1)
+"""
+macro lazy_constraint(model, cb_data, expr)
     code = quote
         lazy_con = @build_constraint $expr
-        GLPK.addrow!(
+        add_lazy_constraint(
+            $model,
             $cb_data,
             JuMP.moi_function(lazy_con.func),
             lazy_con.set
         )
+
     end
     quote
         let
@@ -20,14 +34,80 @@ macro lazy_constraint(cb_data, expr)
     end
 end
 
-function GLPK.ios_heur_sol(cb_data::GLPK.CallbackData,
-                           sol::Dict{JuMP.VariableRef, Float64})
-    GLPK.ios_heur_sol(
-        cb_data,
-        Dict(JuMP.index(variable) => value for (variable, value) in sol)
+"""
+    set_callbacks(model::Model; lazy = nothing, heuristic = nothing)
+
+Set an (optional) lazy and heuristic callback. This can be used only when
+`JuMP.mode(mode) == DIRECT`.
+
+Each callback must be a function that takes two arguments: the backend
+`optimizer` object (returned by `JuMP.backend(model)`), and a solver-specific
+type `cb_data`.
+
+### Example
+
+    set_callbacks(model;
+        lazy = (optimizer, cb_data) -> println("Called from lazy callback.")
     )
-    return
+"""
+function set_callbacks(model::Model; lazy = nothing, heuristic = nothing)
+    if JuMP.mode(model) != JuMP.DIRECT
+        error("You must use a solver in DIRECT mode to use callbacks in JuMP.")
+    end
+    _set_callbacks(model, JuMP.backend(model), lazy, heuristic)
 end
 
+function _set_callbacks(model, optimizer, lazy, heuristic)
+    error("The model $(typeof(optimizer)) does not support callbacks.")
+end
+
+"""
+    add_lazy_constraint(model, cb_data, func, set)
+
+Add a lazy constraint `func`-in-`set` to `model`.
+
+This can be called only from a lazy callback set by `set_callbacks`.
+"""
+function add_lazy_constraint end
+
+
+"""
+    add_heuristic_solution(model, cb_data, sol::Dict{JuMP.VariableRef, Float64})
+
+Provide the heuristic solution given by the variable-value mapping of `sol` to
+`model`.
+
+This can be called only from a heuristic callback set by `set_callbacks`.
+"""
+function add_heuristic_solution end
+
+###
+### GLPK specific functionality.
+###
+
+function _set_callbacks(model::Model, optimizer::GLPK.Optimizer,
+                        lazy_callback, heuristic_callback)
+    MOI.set(optimizer, GLPK.CallbackFunction(), (cb_data) -> begin
+        reason = GLPK.ios_reason(cb_data.tree)
+        if lazy_callback !== nothing && reason == GLPK.IROWGEN
+            GLPK.get_col_prim(cb_data)
+            lazy_callback(model, cb_data)
+        elseif heuristic_callback !== nothing && reason == GLPK.IHEUR
+            GLPK.get_col_prim(cb_data)
+            heuristic_callback(model, cb_data)
+        end
+        return
+    end)
+end
+
+function add_lazy_constraint(model::Model, cb_data::GLPK.CallbackData, func, set)
+    GLPK.addrow!(cb_data, func, set)
+end
+
+function add_heuristic_solution(model::Model, cb_data::GLPK.CallbackData,
+                                sol::Dict{JuMP.VariableRef, Float64})
+    GLPK.ios_heur_sol(cb_data, Dict(
+        JuMP.index(variable) => value for (variable, value) in sol))
+end
 
 end
--- test/callbacks.jl
+++ test/callbacks.jl
@@ -16,55 +16,49 @@ model = JuMP.direct_model(GLPK.Optimizer())
 
 # This vector is going to cache the value of `cb_where` everytime our callback
 # gets called.
-cb_calls = Int32[]
+cb_calls = Symbol[]
 
-# Here is the callback function. We set the solver-specific attribute
-# `Gurobi.CallbackFunction()`, passing a function (in this case, anonymous) as
-# the third argument.
-MOI.set(model, GLPK.CallbackFunction(), (cb_data) -> begin
-    reason = GLPK.ios_reason(cb_data.tree)
-    # Cache the value of `cb_where` in `cb_calls`. Note how we can access
-    # variables in the outer scope.
-    push!(cb_calls, reason)
-    if reason == GLPK.IROWGEN
-        GLPK.get_col_prim(cb_data)
-        # Double check for sanity's sake that we have a feasible (given the
-        # current constraints) point.
-        @assert JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-        # Get the values of x and y.
-        x_val, y_val = JuMP.value(x), JuMP.value(y)
-        # Add the lazy constraints using the `@lazy_constraint` macro that was
-        # loaded by `GLPK.@load_extensions`.
-        if y_val - x_val > 1.1 + 1e-6
-            @lazy_constraint(cb_data, y <= 1.1 + x)
-        elseif y_val + x_val > 3 + 1e-6
-            @lazy_constraint(cb_data, y <= 3 - x)
-        end
-    elseif reason == GLPK.IHEUR
-        # GLPK has a fractional solution to the current problem.
-        # Load the solution into the model.
-        GLPK.get_col_prim(cb_data)
-        # Double check for sanity's sake that we have a feasible (given the
-        # current constraints) point.
-        @assert JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-        # Get the values of x and y.
-        x_val, y_val = JuMP.value(x), JuMP.value(y)
-        # Provide a heuristic solution.
-        GLPK.ios_heur_sol(cb_data, Dict(x => 1.0, y => 2.0))
+function my_lazy_callback(model, cb_data)
+    push!(cb_calls, :lazy)
+    # Double check for sanity's sake that we have a feasible (given the
+    # current constraints) point.
+    @assert primal_status(model) == MOI.FEASIBLE_POINT
+    # Get the values of x and y.
+    x_val, y_val = value(x), value(y)
+    # Add the lazy constraints using the `@lazy_constraint` macro that was
+    # loaded by `@load_extensions`.
+    if y_val - x_val > 1.1 + 1e-6
+        @lazy_constraint(model, cb_data, y <= 1.1 + x)
+    elseif y_val + x_val > 3 + 1e-6
+        @lazy_constraint(model, cb_data, y <= 3 - x)
     end
-    return
-end)
+end
+
+function my_heuristic_callback(model, cb_data)
+    push!(cb_calls, :heuristic)
+    # Double check for sanity's sake that we have a feasible (given the
+    # current constraints) point.
+    @assert primal_status(model) == MOI.FEASIBLE_POINT
+    # Get the values of x and y.
+    x_val, y_val = value(x), value(y)
+    # Provide a heuristic solution.
+    add_heuristic_solution(
+        model, cb_data, Dict(x => 1.0, y => 2.0))
+end
+
+set_callbacks(model;
+    lazy = my_lazy_callback,
+    heuristic = my_heuristic_callback
+)
 
 # Solve the model.
-JuMP.optimize!(model)
+optimize!(model)
 
 # Check the solution.
-@test JuMP.value(x) == 1
-@test JuMP.value(y) == 2
+@test value(x) == 1
+@test value(y) == 2
 
 # Check that our callback has been used.
 @test length(cb_calls) > 0
-@test GLPK.ISELECT in cb_calls
-@test GLPK.IPREPRO in cb_calls
-@test GLPK.IROWGEN in cb_calls
-@test GLPK.IHEUR in cb_calls
+@test :lazy in cb_calls
+@test :heuristic in cb_calls
