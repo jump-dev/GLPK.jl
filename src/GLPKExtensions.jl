6600aca413c1cd48dc94eacd243cc67a11bb2905
odow <o.dowson@gmail.com>
2019-02-11 08:26:47 -0600

    Remove dependence on JuMP.Model

--- src/GLPKExtensions.jl
+++ src/GLPKExtensions.jl
@@ -2,28 +2,29 @@ module GLPKJuMPExtensions
 
 using GLPK, JuMP
 
-function JuMP._set_callbacks(model::Model, optimizer::GLPK.Optimizer,
+function JuMP._set_callbacks(optimizer::GLPK.Optimizer,
                              lazy_callback, heuristic_callback)
     MOI.set(optimizer, GLPK.CallbackFunction(), (cb_data) -> begin
         reason = GLPK.ios_reason(cb_data.tree)
         if lazy_callback !== nothing && reason == GLPK.IROWGEN
             GLPK.get_col_prim(cb_data)
-            lazy_callback(model, cb_data)
+            lazy_callback(cb_data)
         elseif heuristic_callback !== nothing && reason == GLPK.IHEUR
             GLPK.get_col_prim(cb_data)
-            heuristic_callback(model, cb_data)
+            heuristic_callback(cb_data)
         end
         return
     end)
 end
 
 function JuMP.add_lazy_constraint(
-        model::Model, cb_data::GLPK.CallbackData, func, set)
+        ::GLPK.Optimizer, cb_data::GLPK.CallbackData, func, set)
     GLPK.addrow!(cb_data, func, set)
 end
 
-function JuMP.add_heuristic_solution(model::Model, cb_data::GLPK.CallbackData,
-                                     sol::Dict{JuMP.VariableRef, Float64})
+function JuMP.add_heuristic_solution(
+        ::GLPK.Optimizer, cb_data::GLPK.CallbackData,
+        sol::Dict{JuMP.VariableRef, Float64})
     GLPK.ios_heur_sol(cb_data, Dict(
         JuMP.index(variable) => value for (variable, value) in sol))
 end
