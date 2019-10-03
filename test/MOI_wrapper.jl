59c3d525d173f49b1b02353f2de0677d4906b11d
odow <o.dowson@gmail.com>
2019-10-02 19:44:28 -0500

    Updates for MOI 0.9.4

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -14,8 +14,15 @@ const CONFIG = MOIT.TestConfig()
         # FIXME `NumberOfThreads` not supported
         "number_threads",
         # These are excluded because GLPK does not support quadratics.
-        "solve_qp_edge_cases", "solve_qcp_edge_cases", "delete_soc_variables",
-        "solve_zero_one_with_bounds_3"
+        "solve_qcp_edge_cases",
+        "solve_qp_edge_cases",
+        "delete_soc_variables",
+
+        # Tested below because the termination status is different.
+        "solve_zero_one_with_bounds_3",
+
+        # TODO(odow): not implemented.
+        "number_threads",
     ])
     @testset "solve_zero_one_with_bounds_3" begin
         MOI.empty!(OPTIMIZER)
