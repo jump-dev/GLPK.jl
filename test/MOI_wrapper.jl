b83d7eb48136bf359a183c562c7d36e10a316954
Benoît Legat <benoit.legat@gmail.com>
2019-10-13 19:18:23 +0200

    Update to MOI v0.9.5

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -11,8 +11,10 @@ const CONFIG = MOIT.TestConfig()
 @testset "Unit Tests" begin
     MOIT.basic_constraint_tests(OPTIMIZER, CONFIG)
     MOIT.unittest(OPTIMIZER, CONFIG, [
+        # FIXME `NumberOfThreads` not supported
+        "number_threads",
         # These are excluded because GLPK does not support quadratics.
-        "solve_qp_edge_cases", "solve_qcp_edge_cases",
+        "solve_qp_edge_cases", "solve_qcp_edge_cases", "delete_soc_variables",
         "solve_zero_one_with_bounds_3"
     ])
     @testset "solve_zero_one_with_bounds_3" begin
