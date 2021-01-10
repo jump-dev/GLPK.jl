a62665d4461a069d61662ad91fdffb293a2905fc
Oscar Dowson <odow@users.noreply.github.com>
2021-01-11 10:40:55 +1300

    Deprecate passing kwargs to Optimizer (#166)

--- src/MOI_wrapper/MOI_wrapper.jl
+++ src/MOI_wrapper/MOI_wrapper.jl
@@ -69,7 +69,6 @@ end
 mutable struct Optimizer <: MOI.AbstractOptimizer
     # The low-level GLPK problem.
     inner::Ptr{glp_prob}
-    presolve::Bool
     method::MethodEnum
 
     interior_param::glp_iptcp
@@ -131,7 +130,7 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     Optimizer(;
         want_infeasibility_certificates::Bool = true,
         method::MethodEnum = GLPK.SIMPLEX,
-        )
+    )
 
     Create a new Optimizer object.
 
@@ -149,12 +148,10 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     function Optimizer(;
         want_infeasibility_certificates::Bool = true,
         method::MethodEnum = SIMPLEX,
-        presolve::Bool = false,
         kwargs...,
     )
         model = new()
         model.inner = glp_create_prob()
-        model.presolve = presolve
         model.method = method
         model.want_infeasibility_certificates = want_infeasibility_certificates
 
@@ -166,8 +163,12 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
         glp_init_smcp(model.simplex_param)
 
         MOI.set(model, MOI.RawParameter("msg_lev"), GLP_MSG_ERR)
-        if model.presolve
-            MOI.set(model, MOI.RawParameter("presolve"), GLP_ON)
+        if length(kwargs) > 0
+            @warn(
+                "Passing parameters as keyword arguments is deprecated. Use " *
+                "`JuMP.set_optimizer_attribute` or `MOI.RawParameter(key)` " *
+                "instead."
+            )
         end
         for (key, val) in kwargs
             MOI.set(model, MOI.RawParameter(String(key)), val)
