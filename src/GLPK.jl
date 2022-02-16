66abf3d8e5bb4665951ddb3e80c68731fd917042
Oscar Dowson <odow@users.noreply.github.com>
2022-02-17 09:15:09 +1300

    [breaking] prep for 1.0 release (#209)

--- src/GLPK.jl
+++ src/GLPK.jl
@@ -1,22 +1,31 @@
 module GLPK
 
-if haskey(ENV, "JULIA_GLPK_LIBRARY_PATH") || VERSION < v"1.3"
-    deps_file = joinpath(dirname(@__DIR__), "deps", "deps.jl")
-    if isfile(deps_file)
-        include(deps_file)
-    else
-        error(
-            "GLPK not properly installed. Please run " *
-            "`import Pkg; Pkg.build(\"GLPK\")`.",
-        )
+import GLPK_jll
+
+function __init__()
+    global libglpk = GLPK_jll.libglpk
+    p = glp_version()
+    version = VersionNumber(parse.(Int, split(unsafe_string(p), "."))...)
+    if !(v"4.64.0" <= version <= v"5.0.0")
+        error("""
+        You have installed version $version of GLPK, which is not supported
+        by GLPK.jl. We prefer GLPK version 5.0, but this may also work on versions
+        4.64 and newer.
+
+        After installing GLPK 5.0, run:
+
+            import Pkg
+            Pkg.rm("GLPK")
+            Pkg.add("GLPK")
+
+        If you have a newer version of GLPK installed, changes may need to be made
+        to the Julia code. Please open an issue at
+        https://github.com/jump-dev/GLPK.jl.
+        """)
     end
-else
-    import GLPK_jll: libglpk
+    return
 end
 
-using CEnum
-
-include("gen/ctypes.jl")
 include("gen/libglpk_common.jl")
 include("gen/libglpk_api.jl")
 
@@ -36,33 +45,9 @@ See the GLPK manual for more details.
 """
 offset(x::Vector) = Ref(x, 0)
 
-const _GLPK_VERSION = let
-    p = glp_version()
-    VersionNumber(parse.(Int, split(unsafe_string(p), "."))...)
-end
-
-if !(v"4.64.0" <= _GLPK_VERSION <= v"5.0.0")
-    error("""
-    You have installed version $_GLPK_VERSION of GLPK, which is not supported
-    by GLPK.jl. We prefer GLPK version 5.0, but this may also work on versions
-    4.64 and newer.
-
-    After installing GLPK 5.0, run:
-
-        import Pkg
-        Pkg.rm("GLPK")
-        Pkg.add("GLPK")
-
-    If you have a newer version of GLPK installed, changes may need to be made
-    to the Julia code. Please open an issue at
-    https://github.com/jump-dev/GLPK.jl.
-    """)
-end
-
 include("MOI_wrapper/MOI_wrapper.jl")
 include("MOI_wrapper/MOI_copy.jl")
 include("MOI_wrapper/MOI_callbacks.jl")
-include("MOI_wrapper/deprecated_constants.jl")
 
 # GLPK exports all `GLP_XXX` and `glp_xxx` symbols. If you don't want all of
 # these symbols in your environment, then use `import GLPK` instead of
@@ -75,9 +60,7 @@ for sym in names(@__MODULE__, all = true)
     end
 end
 
-if Base.VERSION >= v"1.4.2"
-    include("precompile.jl")
-    _precompile_()
-end
+include("precompile.jl")
+_precompile_()
 
 end
